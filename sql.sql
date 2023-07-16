596acb396b7753e425614a9324a236bed94f1588
Noel Guillen <noelblas274@gmail.com>
2023-07-15 22:46:50 -0400

    database table created

---
+++ sql.sql
@@ -0,0 +1,6 @@
+-- CREATE TABLE USERS (username TEXT NOT NULL PRIMARY KEY, password NOT NULL, email NOT NULL);
+
+-- INSERT INTO USERS (username, password, email)
+-- VALUES ('TINA', 'BYE WORLD','tinawinnnnn@gmail.com')
+
+SELECT * FROM USERS;
